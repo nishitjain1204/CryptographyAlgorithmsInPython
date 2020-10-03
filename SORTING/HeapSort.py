6082c198eb8a93a45b0e7ef3ea4bca9ba55e81e6
Double 77 <danielread55@hotmail.co.uk>
2020-10-03 17:00:14 +0100

    Cleaned Up Some Bits of the Code & Added Comment
    Cleaned up the code and added some commentary.

--- SORTING/HeapSort.py
+++ SORTING/HeapSort.py
@@ -1,31 +1,47 @@
-def heap(arr, n, i): 
+# Re-arrange the heap to maintain the heap property - I.e. 'Heapify' With n being the size of the heap & i being the largest as the root
+def Heapify(arr, n, i): 
     largest = i  
     left = 2 * i + 1     
     right = 2 * i + 2    
+    
+    # Check if the left child of the root exists and is greater than the root 
     if left < n and arr[i] < arr[left]: 
         largest = left
-  
+
+    # Check if the right child of the root exists and is greater than the root 
     if right < n and arr[largest] < arr[right]: 
         largest = right
 
+    # Change the root if i was not the largest
     if largest != i: 
         arr[i],arr[largest] = arr[largest],arr[i] 
-  
-        heap(arr, n, largest) 
+        # 'Heapify the root'
+        Heapify(arr, n, largest) 
 
+# Function to sort the given array
 def HeapSort(arr): 
     n = len(arr) 
- 
+    
+    # Build a Max Heap
     for i in range(n // 2 - 1, -1, -1): 
-        heap(arr, n, i) 
-  
+        Heapify(arr, n, i) 
+    
+    # Extract the elements one by one
     for i in range(n-1, 0, -1): 
         arr[i], arr[0] = arr[0], arr[i]  
-        heap(arr, i, 0)   
+        Heapify(arr, i, 0)   
 
-array = [ 12, 11, 13, 5, 6, 7] 
-HeapSort(array) 
-n = len(array) 
-print ("The Sorted array is") 
-for i in range(n): 
-    print ("%d" %array[i]), 
+    # Print out the sorted array
+    n = len(array) 
+    print ("The Sorted Array Is:") 
+    for i in range(n): 
+        print ("%d" %array[i]) 
+
+
+array = [12, 11, 13, 5, 6, 7]
+HeapSort(array)
+
+
+
+
+
