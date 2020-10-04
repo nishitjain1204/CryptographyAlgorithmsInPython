940cd87571c89a45454db543ae32f1bfaf452538
Double 77 <danielread55@hotmail.co.uk>
2020-10-04 16:32:51 +0100

    Updated HeapSort.py
    - Added user input for the array: prompts for a set of numbers which is to then be sorted via the heap algorithm
    - Wrapped the main function in a function main()

--- SORTING/HeapSort.py
+++ SORTING/HeapSort.py
@@ -1,5 +1,5 @@
 # Re-arrange the heap to maintain the heap property - I.e. 'Heapify' With n being the size of the heap & i being the largest as the root
-def Heapify(arr, n, i): 
+def main(arr, n, i): 
     largest = i  
     left = 2 * i + 1     
     right = 2 * i + 2    
@@ -16,7 +16,7 @@ def Heapify(arr, n, i):
     if largest != i: 
         arr[i],arr[largest] = arr[largest],arr[i] 
         # 'Heapify the root'
-        Heapify(arr, n, largest) 
+        main(arr, n, largest) 
 
 # Function to sort the given array
 def HeapSort(arr): 
@@ -24,12 +24,12 @@ def HeapSort(arr):
     
     # Build a Max Heap
     for i in range(n // 2 - 1, -1, -1): 
-        Heapify(arr, n, i) 
+        main(arr, n, i) 
     
     # Extract the elements one by one
     for i in range(n-1, 0, -1): 
         arr[i], arr[0] = arr[0], arr[i]  
-        Heapify(arr, i, 0)   
+        main(arr, i, 0)   
 
     # Print out the sorted array
     n = len(array) 
@@ -37,11 +37,7 @@ def HeapSort(arr):
     for i in range(n): 
         print ("%d" %array[i]) 
 
-
-array = [12, 11, 13, 5, 6, 7]
-HeapSort(array)
-
-
-
-
-
+if __name__ == "__main__":
+    print("Please Enter The Elements One After The Other Separated By A Space:")
+    array = list(map(int,input().split()))
+    HeapSort(array)
